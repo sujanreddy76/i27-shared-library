539268f11c67e13ea2c285b6cabe90972a4bdd52
sujan <sujan.reddy.76@gmail.com>
2026-05-28 00:53:38 +0530

    changed Dockerfile path

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -221,7 +221,7 @@ def imageValidation() {
 def dockerBuildAndPush(){
     return {
         echo "**************** Building Docker Image *******************"
-        sh "docker build --no-cache -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd/"
+        sh "docker build --no-cache -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd/Dockerfile"
         echo "**************** Login to docker registry *******************"
         sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
         echo "**************** Push Image to docker registry *******************"
